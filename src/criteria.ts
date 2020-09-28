1a0b49726a71efdc0200197de5c3d183272cc96b
R Gastmeier <r.gastmeier@gmail.com>
2020-09-28 20:52:20 +0200

    Improve interface structure

--- src/criteria.ts
+++ src/criteria.ts
@@ -1,33 +1,33 @@
-export interface Criteria {
+export interface BaseCriteria<ThisType> {
   [ field: string ]: 
     any | // a value
     [] | // an array of values
     OperatorAndValue | // an operator value pair
     OperatorAndValue[] | // an array of operator value pairs
-    {[ field: string ]: Criteria } // a field which contains Criteria and thus is referencing another entity
+    { [ field: string ]: ThisType } // a field which contains Criteria and thus is referencing another entity
 }
+
+export interface Criteria extends BaseCriteria<Criteria> {}
 
 export interface CreateCriteria {
   [ field: string ]: any
 }
 
-export interface ReadCriteria extends Criteria {
+export interface ReadCriteria extends BaseCriteria<ReadCriteria> {
   orderBy?: string | string[] | { field: string, direction?: OrderDirection } | { field: string, direction?: OrderDirection }[]
   limit?: number
   offset?: number
 }
 
 export interface UpdateCriteria extends Criteria {
-  set: {
-    [ field: string ]: any
-  }
+  set: { [ field: string ]: any }
 }
 
 export interface DeleteCriteria extends Criteria {}
 
 export interface OperatorAndValue {
   operator: string
-  value: any|[]
+  value: any | [] // a value or an array of values
 }
 
 export function isOperatorAndValue(value: any): boolean {
