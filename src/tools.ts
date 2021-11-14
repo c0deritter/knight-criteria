53288bfaf88b71e917b4a0f978107cdf3d24811a
René Gastmeier <rene@coderitter.io>
2021-11-14 20:24:27 +0100

    Add summarize function

--- src/tools.ts
+++ src/tools.ts
@@ -1,4 +1,4 @@
-import { CriteriaObject } from './criteria'
+import { Criteria, CriteriaObject } from './criteria'
 
 export function isCriteriaEmpty(criteria: CriteriaObject) {
   for (let property of Object.keys(criteria)) {
@@ -12,4 +12,43 @@ export function isCriteriaEmpty(criteria: CriteriaObject) {
 
 export function isComparison(value: any): boolean {
   return typeof value == 'object' && value !== null && value['@operator'] != undefined
+}
+
+/**
+ * Since criteria can be an array of criteria objects, it is possible to define multiple
+ * @-properties like `@limit` or `@load`. Since it is only possible to process one
+ * @-property of the same kind, this function summarizes all the @-properties.
+ * 
+ * If there are two @-properties of the same kind but with different values, the first
+ * one is taken.
+ * 
+ * @param criteria 
+ * @param summarized 
+ * @returns 
+ */
+export function summarize(criteria: Criteria, summarized: any = {}): CriteriaObject {
+  if (criteria instanceof Array) {
+    for (let criterium of criteria) {
+      if (typeof criterium == 'object') {
+        summarize(criterium, summarized)
+      }
+    }
+  }
+  
+  else if (typeof criteria == 'object') {
+    for (let key of Object.keys(criteria)) {
+      if (! (key in summarized) && key[0] == '@') {
+        summarized[key] = criteria[key]
+      }
+      else if (typeof criteria[key] == 'object') {
+        if (! (key in summarized)) {
+          summarized[key] = {}
+        }
+
+        summarize(criteria[key], summarized[key])
+      }
+    }
+  }
+
+  return summarized
 }
--- test/tools.test.ts
+++ test/tools.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 import 'mocha'
-import { isCriteriaEmpty, isOperatorAndValue } from '../src/tools'
+import { isComparison, isCriteriaEmpty, summarize } from '../src/tools'
 
 describe('isCriteriaEmpty', function() {
   it('should return false if criteria is not empty', function() {
@@ -13,26 +13,177 @@ describe('isCriteriaEmpty', function() {
   })
 })
 
-describe('isValueOperator', function() {
+describe('isComparison', function() {
   it('should return false for any value other than a non null object', function() {
-    expect(isOperatorAndValue(0)).to.be.false
-    expect(isOperatorAndValue(true)).to.be.false
-    expect(isOperatorAndValue(false)).to.be.false
-    expect(isOperatorAndValue('')).to.be.false
-    expect(isOperatorAndValue(null)).to.be.false
-    expect(isOperatorAndValue(undefined)).to.be.false
-    expect(isOperatorAndValue(NaN)).to.be.false
+    expect(isComparison(0)).to.be.false
+    expect(isComparison(true)).to.be.false
+    expect(isComparison(false)).to.be.false
+    expect(isComparison('')).to.be.false
+    expect(isComparison(null)).to.be.false
+    expect(isComparison(undefined)).to.be.false
+    expect(isComparison(NaN)).to.be.false
   })
 
   it('should return false for a wrong object', function() {
-    expect(isOperatorAndValue({})).to.be.false
-    expect(isOperatorAndValue({ value: '', a: '' })).to.be.false
-    expect(isOperatorAndValue({ operator: '' })).to.be.false
-    expect(isOperatorAndValue({ value: '', operator: '', a: ''})).to.be.false
+    expect(isComparison({})).to.be.false
+    expect(isComparison({ value: '', a: '' })).to.be.false
+    expect(isComparison({ operator: '' })).to.be.false
+    expect(isComparison({ value: '', operator: '', a: ''})).to.be.false
   })
 
   it('should return true for a correct object', function() {
-    expect(isOperatorAndValue({ value: '' })).to.be.true
-    expect(isOperatorAndValue({ value: '', operator: '' })).to.be.true
+    expect(isComparison({ value: '' })).to.be.true
+    expect(isComparison({ value: '', operator: '' })).to.be.true
+  })
+})
+
+describe('summarize', function() {
+  it('should return an empty criteria object if an empty criteria object was given', function() {
+    expect(summarize({})).to.deep.equal({})
+  })
+
+  it('should return an empty criteria object if an empty array was given', function() {
+    expect(summarize([])).to.deep.equal({})
+  })
+
+  it('should return the meta data of a criteria object', function() {
+    expect(summarize({
+      property1: 'a',
+      property2: 1,
+      '@orderBy': 'property1',
+      '@limit': 10,
+      '@offset': 20
+    })).to.deep.equal({
+      '@orderBy': 'property1',
+      '@limit': 10,
+      '@offset': 20
+    })
+  })
+
+  it('should regard relationships', function() {
+    expect(summarize({
+      property1: 'a',
+      property2: 1,
+      '@orderBy': 'property1',
+      '@limit': 10,
+      '@offset': 20,
+      object1: {
+        property3: 'b',
+        property4: 2,
+        '@load': true,
+        '@orderBy': 'property3',
+        '@limit': 30,
+        '@offset': 40
+      }
+    })).to.deep.equal({
+      '@orderBy': 'property1',
+      '@limit': 10,
+      '@offset': 20,
+      object1: {
+        '@load': true,
+        '@orderBy': 'property3',
+        '@limit': 30,
+        '@offset': 40
+      }
+    })
+  })
+
+  it('should return the meta data of a criteria array', function() {
+    expect(summarize([[
+      {
+        property1: 'a',
+        property2: 1,
+        '@orderBy': 'property1',
+        '@limit': 10,
+        '@offset': 20,
+        object1: {
+          property3: 'b',
+          property4: 2,
+          '@load': true,
+          '@orderBy': 'property3',
+          '@limit': 30,
+          '@offset': 40
+        }
+      }
+    ]])).to.deep.equal({
+      '@orderBy': 'property1',
+      '@limit': 10,
+      '@offset': 20,
+      object1: {
+        '@load': true,
+        '@orderBy': 'property3',
+        '@limit': 30,
+        '@offset': 40
+      }
+    })
+  })
+
+  it('should combine meta data from multiple criteria objects but not overwrite once made settings', function() {
+    expect(summarize([
+      {
+        '@orderBy': 'property1',
+        '@limit': 10,
+        '@offset': 20,
+        object1: {
+          '@load': true,
+          '@orderBy': 'property3',
+          '@limit': 30,
+          '@offset': 40
+        }
+      },
+      'AND',
+      {
+        '@orderBy': 'property2',
+        '@limit': 11,
+        '@offset': 21,
+        object1: {
+          '@load': false,
+          '@orderBy': 'property4',
+          '@limit': 31,
+          '@offset': 41
+        }
+      }
+    ])).to.deep.equal({
+      '@orderBy': 'property1',
+      '@limit': 10,
+      '@offset': 20,
+      object1: {
+        '@load': true,
+        '@orderBy': 'property3',
+        '@limit': 30,
+        '@offset': 40
+      }
+    })
+  })
+
+  it('should combine meta data from multiple criteria objects and add missing settings', function() {
+    expect(summarize([
+      {
+        object1: {
+        }
+      },
+      'AND',
+      {
+        '@orderBy': 'property2',
+        '@limit': 11,
+        '@offset': 21,
+        object1: {
+          '@load': false,
+          '@orderBy': 'property4',
+          '@limit': 31,
+          '@offset': 41
+        }
+      }
+    ])).to.deep.equal({
+      '@orderBy': 'property2',
+      '@limit': 11,
+      '@offset': 21,
+      object1: {
+        '@load': false,
+        '@orderBy': 'property4',
+        '@limit': 31,
+        '@offset': 41
+      }
+    })
   })
 })
