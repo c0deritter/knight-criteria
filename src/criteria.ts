34c5a49ab13c7a1583846257f796ccd986d63ae1
René Gastmeier <rene@coderitter.io>
2022-05-21 10:41:30 +0200

    Add interface ValueCriteriaField; Add generic type to Comparison

--- src/criteria.ts
+++ src/criteria.ts
@@ -8,21 +8,7 @@ export interface CriteriaObject {
   '@min'?: number|Comparison
   '@max'?: number|Comparison
 
-  [field: string]:
-    // a value
-    any |
-
-    // an array of values
-    any[] |
-
-    // a comparison
-    Comparison |
-
-    // an array of comparisons combined with logical operators
-    (Comparison | 'AND' | 'OR' | 'XOR')[] |
-
-    // a relationship
-    CriteriaObject
+  [field: string]: ValueCriteriaField | Criteria
 
   '@orderBy'?:
     string |
@@ -33,10 +19,23 @@ export interface CriteriaObject {
   '@offset'?: number
 }
 
-export interface Comparison {
+export type ValueCriteriaField<T = any> =  
+  // a value
+  T |
+
+  // an array of values
+  T[] |
+
+  // a comparison
+  Comparison<T> |
+
+  // an array of comparisons combined with logical operators
+  (Comparison<T> | 'AND' | 'OR' | 'XOR')[]
+
+export interface Comparison<T = any> {
   '@not'?: boolean
   '@operator': string|Operator
-  '@value'?: any | [] // a value or an array of values
+  '@value'?: T | T[] // a value or an array of values
 }
 
 export interface OrderBy {
